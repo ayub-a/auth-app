70f8cbef1c01fd229b246369e7f520faa626ec44
ayub-a <ayub-dev@yandex.ru>
2025-05-16 17:10:57 +0500

    feat(server/auth): complete email send & verification

--- backend/src/services/auth.service/createAccount.ts
+++ backend/src/services/auth.service/createAccount.ts
@@ -1,4 +1,5 @@
 import { HTTP_STATUS } from "../../constants/http"
+import { APP_ORIGIN } from "../../constants/env"
 import { EVerificationCodeType } from "../../types/verificationCodeType"
 
 import { SessionModel } from "../../models/session.model"
@@ -7,6 +8,7 @@ import { VerificationCodeModel } from "../../models/verificationCode.model"
 
 import { appAssert } from "../../utils/appAssert"
 import { TimeUtils } from "../../utils/date"
+import { EmailUtils, emailUtils } from "../../utils/sendEmail"
 
 import { tokenService } from "../token.service"
 import { IAuthParams } from "."
@@ -15,7 +17,6 @@ import { IAuthParams } from "."
 export async function createAccount(data: IAuthParams) {
 
     const isUserExist = await UserModel.exists({ email: data.email })
-
     appAssert(!isUserExist, HTTP_STATUS.CONFLICT, 'Email already in use')
 
     const user = await UserModel.create({
@@ -23,12 +24,18 @@ export async function createAccount(data: IAuthParams) {
         password: data.password
     })
 
-    // send verification email
+
     const verificationEmail = await VerificationCodeModel.create({
         userId: user._id,
         type: EVerificationCodeType.EmailVerification,
         expiresAt: TimeUtils.thirtyDaysFromNow
     })
+
+    const url = `${APP_ORIGIN}/email/verify/${verificationEmail._id}`
+
+    const { error } = await emailUtils.sendEmail({ to: user.email, ...EmailUtils.getVerifyEmailTemplate(url) })
+    if (error) console.log('SEND_EMAIL_ERROR', error)
+    
 
     const session = await SessionModel.create({
         userId: user._id,
---
+++ backend/src/utils/sendEmail/index.ts
@@ -0,0 +1,58 @@
+import { resend } from "../../config/resend";
+import { EMAIL_SENDER, NODE_ENV } from "../../constants/env";
+
+import { passwordResetTemplate } from "./templates/passwordReset";
+import { verifyEmailTemplate } from "./templates/verifyEmail";
+
+
+interface IEmailParams {
+    to: string
+    subject: string
+    text: string 
+    html: string
+}
+
+
+export class EmailUtils {
+
+    static getPasswordResetTemplate(url: string) {
+        return {
+            subject: "Password Reset Request",
+            text: `You requested a password reset. Click on the link to reset your password: ${url}`,
+            html: passwordResetTemplate(url)        }
+    }
+
+
+    static getVerifyEmailTemplate(url: string) {
+        return {
+            subject: "Verify Email Address",
+            text: `Click on the link to verify your email address: ${url}`,
+            html: verifyEmailTemplate(url)
+        }
+    }
+
+
+    private static get getFromEmail() {
+        return NODE_ENV === 'development' ? 'onboarding@resend.dev' : EMAIL_SENDER
+    }
+
+
+    private static getToEmail(to: string) {
+        return NODE_ENV === 'development' ? 'delivered@resend.dev' : to
+    }
+
+
+    async sendEmail({to, subject, text, html}: IEmailParams) {
+        return await resend.emails.send({
+            from: EmailUtils.getFromEmail,
+            to: EmailUtils.getToEmail(to),
+            subject,
+            text,
+            html,
+        })
+    } 
+
+}
+
+
+export const emailUtils = new EmailUtils()
